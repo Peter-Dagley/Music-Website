c9d83b2a7cd8d59938a2e9aa0331881d113fbf49
Peter-Dagley <peter_dagley@outlook.com>
2022-07-18 18:27:56 +0100

    connected mongo db and added api for reading songs

---
+++ database/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({origin:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.static('script'));
+
+const uri = 'mongodb://localhost:27017/soundrave';
+let opts = { useNewUrlParser: true };
+
+// Routes
+const songsRouter = require("./routes/songRoutes");
+
+app.use("/songs", songsRouter);
+
+mongoose.connect
+(uri, opts, function (error) {
+    if (error) {
+        console.log("Errors: " + error)
+    } else {
+        console.log("connection ready")
+    }
+});
+
+const myserver = app.listen(4000, () =>
+    console.log("Server started on", myserver.address().port)
+);
+
+module.exports = myserver;
+
---
+++ database/routes/songRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const Songs = require("../models/songModels");
+
+// Global variable for id
+let id = 1;
+
+// View all replies
+router.get("/readSongs", (req, res, next) => {
+    Replies.find()
+      .then((results) => res.send(results))
+      .catch((err) => next(err));
+  });
+
+module.exports = router;
