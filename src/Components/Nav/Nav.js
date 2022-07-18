b8ff9347e6b7614c3fbe666c9bbb7e46d1a97462
Peter-Dagley <peter_dagley@outlook.com>
2022-07-18 14:52:04 +0100

    added basic component pages, elaborated home and added a little css

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import About from '../Pages/About/About';
 import Contact from '../Pages/Contact/Contact';
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '../Home/HomeStyles.css'
+import Banner from './Banner'
+import Trending from './Trending';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className='container-fluid'>
+        <div className='container-lg'>
+        
+            <div className='container' id='header'>
+                <Banner />
+            </div>
+
+            <div className='container'>
+                <Trending />
+            </div>
+
+        </div>
+    </div>
+    </>
   )
 }
 
-export default Home
+export default Home;
