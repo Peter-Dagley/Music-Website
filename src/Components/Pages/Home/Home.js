28c5fd35a33a690f7ebbd72e0db16fc4d6c67e22
Peter-Dagley <peter_dagley@outlook.com>
2022-07-21 11:13:07 +0100

    changed layout of homepage to make it an actual readable website

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -6,19 +6,25 @@ import Trending from './Trending';
 const Home = () => {
   return (
     <>
-    <div className='container-fluid'>
+      <div className='container-fluid'>
         <div className='container-lg'>
-        
-            <div className='container' id='header'>
-                <Banner />
+
+          <div className='container' id='mainBody'>
+            <div className='container'>
+              <Banner />
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
 
             <div className='container'>
-                <Trending />
+              <Trending />
             </div>
+          </div>
 
         </div>
-    </div>
+      </div>
     </>
   )
 }
