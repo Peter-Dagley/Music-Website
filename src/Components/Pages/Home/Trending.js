aec4db1d2743b1c9b6564cd0a6d5c1168237afbe
Peter-Dagley <peter_dagley@outlook.com>
2022-07-21 12:26:10 +0100

    rearranged title and album art

--- src/Components/Pages/Home/Trending.js
+++ src/Components/Pages/Home/Trending.js
@@ -25,9 +25,17 @@ const [data, setData] = useState([]);
   // Page Display
   return (
       <>
-        <h1>Album of the Day: {albumTitle}</h1>
+        <div className='container' id='rightbanner'>
+          <div>
+            <img id='image1' src={`${albumArt}`} />
+          </div>
+          
+          <div>
+            <h1>Album of the Day: {albumTitle}</h1>
+            <h2>Artist: {artist}</h2>
+          </div>
+        </div>
 
-        <h2>Artist: {artist}</h2>
 
         {data.map(({id, songTitle, albumTitle, albumArt, artist, genre, length}) =>
           <div key={id} id={id} className='card'>
